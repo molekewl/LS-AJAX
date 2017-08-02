c16f5ef8190ffbedab14dfd255bfb34511b46c94
John Wells <molekewl2017@gmail.com>
2017-08-01 23:23:55 -0700

    added some comments

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { getFriends } from '../actions';
 
 class FriendsList extends Component {
+  // componentDidMount: Most common case when starting AJAX calls to load in 
+  // data for your component. Can call setState: YES
+  // another issue: AJAX request might not resolve before the component mounts. If it did, you'd be trying to setState 
+  // on an unmounted component. Doing DidMount will guarantee there's a component to update
   componentDidMount() {
     this.props.getFriends();
   }
@@ -29,9 +33,9 @@ class FriendsList extends Component {
 
 const mapStateToProps = (state) => {
   return {
+    // state from reducers property name
     friends: state.friends
   };
 };
 
 export default connect(mapStateToProps, { getFriends })(FriendsList);
-// export default FriendsList;
